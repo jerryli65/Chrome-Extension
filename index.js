fb5771a0d5c90121e848bcc48d30cfbb85bdf26f
jerry-li65 <jerry.li65@tamu.edu>
2023-01-15 23:31:45 -0600

    v1, awaiting review

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const inputBtn = document.getElementById("input-btn");
 const pDeciphered = document.getElementById("p-deciphered");
 
 inputBtn.addEventListener("click", function () {
-    pDeciphered.innerHTML = inputText.value;
+    if (inputText.value == "")
+        pDeciphered.innerHTML = "Ill Bill";
+    else
+        pDeciphered.innerHTML = inputText.value;
     localStorage.setItem("numUses", localStorage.getItem(numUses) + 1);
 });
